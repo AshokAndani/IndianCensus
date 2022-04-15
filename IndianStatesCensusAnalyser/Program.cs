216b2ecc1f49b985964c82cff0586547089d8636
PORTAL01\Ashokk <ashokk@nextgen.com>
2022-04-15 17:41:59 +0530

    add

---
+++ IndianStatesCensusAnalyser/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Verify.V2.Service;
+
+namespace IndianStatesCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwilioClient.Init("AC76c5cc6331189f66c3840531c1041461", "da7a762cab6f83fb95850dac003d924d");
+            var veriSID = "VAecb9605eae1bd81e22756cac7802e237";
+            var verificationResource = VerificationResource.Create(
+                    to:"+919901233273",
+                    channel: "sms",
+                    pathServiceSid: veriSID
+                );
+            Console.WriteLine(verificationResource.Status);
+
+        }
+    }
+}
