5c94c9960e11a99e2aced79d0d4c449ca9dc9fef
AshokAndani <60642640+AshokAndani@users.noreply.github.com>
2022-07-09 19:27:09 +0530

    Update Program.cs

--- IndianStatesCensusAnalyser/Program.cs
+++ IndianStatesCensusAnalyser/Program.cs
@@ -8,14 +8,6 @@ namespace IndianStatesCensusAnalyser
     {
         static void Main(string[] args)
         {
-            TwilioClient.Init("AC76c5cc6331189f66c3840531c1041461", "da7a762cab6f83fb95850dac003d924d");
-            var veriSID = "VAecb9605eae1bd81e22756cac7802e237";
-            var verificationResource = VerificationResource.Create(
-                    to:"+919901233273",
-                    channel: "sms",
-                    pathServiceSid: veriSID
-                );
-            Console.WriteLine(verificationResource.Status);
 
         }
     }
